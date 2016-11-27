c4fbe5707cf4397a50c4ecf8f99b8bfc6e2b7410
Jooter <bbb314@gmail.com>
2016-11-28 07:10:36 +1100

    Added test cases for sql

--- test/hyper_string/core_test.clj
+++ test/hyper_string/core_test.clj
@@ -35,3 +35,34 @@
                  ["a" :limit:limit] 
                  {}) 
            "a"))))
+
+(deftest sql-str-test-3
+  (testing "simple sql string 3"
+    (is (= (hstr #(apply str (into [] %))
+
+                 {:from (fn [v _]
+                          (let [[tname alias-1] v]
+                            (str " from " tname " " alias-1)))
+
+                  :where (fn [v _]
+                           (let [[expr col-v] v]
+                             (if (vector? col-v)
+                               (if-not (empty? col-v)
+                                 (str " where " expr 
+                                      " (" (clojure.string/join " " col-v)
+                                      ")")))))
+
+                  :limit  (fn [v _] 
+                            (if v 
+                              (str " limit " v)
+                              v))}
+
+                 ["select" 
+                  :from:from 
+                  :where:where
+                  :limit:limit] 
+
+                 {:from ["tab-1" "t1"]
+                  :where ["c in" [1 2]]
+                  }) 
+           "select from tab-1 t1 where c in (1 2)"))))
